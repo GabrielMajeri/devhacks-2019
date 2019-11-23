1d0f4c57bfc376c12b2a4515fd682faffcb298e5
iustin <iustin@privacysystems.eu>
2019-11-23 15:22:43 +0200

    sa

--- Flask/app.py
+++ Flask/app.py
@@ -13,6 +13,23 @@ def login():
     return render_template('index.html', error=error)
 
 
+@app.route('/cards', methods=['GET', 'POST'])
+def cards():
+    carduri = []
+    carduri.append({
+        'nume': 'Popescu',
+        'ultimele_4_cifre_card': '1234',
+        'valid_date': '10/22',
+        'valoare': 1234.56})
+    carduri.append({
+        'nume': 'Ionescu',
+        'ultimele_4_cifre_card': '4321',
+        'valid_date': '11/24',
+        'valoare': 509.21})
+
+    return render_template('carduri.html', cards=carduri)
+
+
 @app.route('/')
 def index():
     return render_template('dashboard.html')
