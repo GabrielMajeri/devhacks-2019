27a8144566c840f51be4ecbe3215d99854b160de
PnzJust <32747902+PnzJust@users.noreply.github.com>
2019-11-23 19:32:56 +0200

    Add basic site layout
    Co-authored-by: iustin <iustin@privacysystems.eu>
    Co-authored-by: null <cosmin.stefan952@yahoo.com>

--- Flask/app.py
+++ Flask/app.py
@@ -1,9 +1,42 @@
-from flask import Flask, render_template, request, redirect
-app = Flask(__name__)
+from flask import Flask, render_template, request, redirect, url_for
+app = Flask(__name__)   
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect('/')
+    return render_template('index.html', error=error)
+
+
+@app.route('/cards', methods=['GET', 'POST'])
+def cards():
+    carduri = []
+    carduri.append({
+        'nume': 'Popescu',
+        'cifre_card': '1234 5678 9012 34',
+        'valid_date': '10/22',
+        'card': 'raifeissen',
+        'cvv': 123,
+        'valoare': 1234.56})
+    carduri.append({
+        'nume': 'Ionescu',
+        'cifre_card': '4321 1243 2356 8655',
+        'valid_date': '11/24',
+        'card': 'bcr',
+        'cvv': 456,
+        'valoare': 509.21})
+
+    return render_template('carduri.html', cards=carduri)
+
 
 @app.route('/')
 def index():
-	return render_template('index.html', parametru1="Macar", parametru2="merge")
+    return render_template('dashboard.html')
 
 
 
