cdc5ee87343373e15b4590014811c7aa8c448cb6
Cosmin2108 <cosmin.stefan952@yahoo.com>
2019-11-23 21:30:30 +0200

    .

--- Flask/app.py
+++ Flask/app.py
@@ -2,14 +2,14 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
         if request.form['username'] != 'admin' or request.form['password'] != 'admin':
             error = 'Invalid Credentials. Please try again.'
         else:
-            return redirect('/dashboard')
+            return redirect('/')
     return render_template('index.html', error=error)
 
 
@@ -34,41 +34,9 @@ def cards():
     return render_template('carduri.html', cards=carduri)
 
 
-@app.route('/card/<id_card>')
-def card(id_card):
-    # card = carddul cu id_card
-    # tranzactii de card[id_card]
-    tranzactiile = []
-    tranzactiile.append({
-                        'valoare1': 11,
-                        'valoare2': 22,
-                        'valoare3': 33})
-    return render_template('card.html', tranzactii=tranzactiile)
-
-
-@app.route('/dashboard', methods=['GET'])
+@app.route('/')
 def index():
-    carduri = []
-    carduri.append({
-        'nume': 'Popescu',
-        'cifre_card': '1234 5678 9012 34',
-        'valid_date': '10/22',
-        'card': 'raifeissen',
-        'cvv': 123,
-        'valoare': 1234.56})
-    carduri.append({
-        'nume': 'Ionescu',
-        'cifre_card': '4321 1243 2356 8655',
-        'valid_date': '11/24',
-        'card': 'bcr',
-        'cvv': 456,
-        'valoare': 509.21})
-    return render_template('dashboard.html', cards=carduri)
-
-
-@app.route('/dashboard', methods=['POST'])
-def handle_data():
-    print ('2')
+    return render_template('dashboard.html')
 
 
 
--- Flask/static/js/add_account.js
+++ Flask/static/js/add_account.js
@@ -1,5 +1,12 @@
+var form = document.getElementById("card_info");
+var span = document.getElementsByClassName("closeButton")[0];
 
 function add_account(){
+  form.style.display = "block";
+}
 
-  var 
+
+span.onclick = function(){
+
+  form.style.display = "none";
 }
